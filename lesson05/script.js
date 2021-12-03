67c87e4498fcee26d16c6d8705d16854c11962cf
Vadim Melnichuk <vad.mk@ya.ru>
2021-12-03 20:19:11 +0300

    Modified conditions and added comments

--- lesson05/script.js
+++ lesson05/script.js
@@ -1,18 +1,29 @@
-let arr = ['449', '17', '787', '224', '689', '48', '262']
+'use strict'
 
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i].startsWith(2) || arr[i].startsWith(4)) {
-    console.log(arr[i]);
+// Поиск чисел, начинающихся с 2 или 4
+const arrNumbers = function () {
+  let arr = ['449', '17', '787', '224', '689', '48', '262']
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i][0] === '2' || arr[i][0] === '4') {
+      console.log(arr[i]);
+    }
   }
 }
+
+arrNumbers();
 
 console.log('===========================');
 
-for (let i = 1; i <= 100; i++) {
-
-  if (i === 1) {
-    console.log(i, "Делители этого числа: 1");
-  } else {
-    console.log(i, "Делители этого числа: 1 и " + i);
+// Вывод делителей простых чисел
+const simpleNumbers = function () {
+  for (let i = 1; i <= 100; i++) {
+    if (i === 1) {
+      console.log(i, "Делитель этого числа: 1");
+    } else {
+      console.log(i, "Делители этого числа: 1 и " + i);
+    }
   }
 }
+
+simpleNumbers();
